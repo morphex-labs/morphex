8683f2a5352b0a402fba4350e9a048305f454520
nikmace <nikmacentosh@gmail.com>
2022-10-23 15:42:18 +0300

    Messages contants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,10 @@ export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
   : 'http://localhost:3000';
 
 export const URL = `${BASE_URL}/api/trpc`;
+
+export const Messages = {
+  MORPHEX_REWARDS: 'Morphex Rewards',
+  BORROWING_APR_FEE: 'Borrowing APR (Fee)',
+  NET_BORROWING_APR: 'Net Borrowing APR',
+  NET_ASSET_VALUE: 'Net Asset Value',
+};
