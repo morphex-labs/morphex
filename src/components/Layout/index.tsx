340a379fb7803a5aca93fccf8a72cf74a7f29ef0
nikmace <nikmacentosh@gmail.com>
2022-10-25 16:30:33 +0300

    Improved redux logic and refactored code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { toggleMenu, togglePopup } from '../../redux/navigation/slice';
 import { isWalletInstalled } from '../../utils/wallet';
 import { MetamaskNotFound } from '../dialogs/MetamaskNotFound';
 import Footer from '../Footer';
@@ -8,19 +6,12 @@ import Header from '../Header';
 import PopupOuter from './PopupOuter';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const dispatch = useDispatch();
-
   const isWalletPresent = isWalletInstalled();
-
-  const handleOutsideClick = () => {
-    dispatch(togglePopup({ value: false }));
-    dispatch(toggleMenu({ value: false }));
-  };
 
   return (
     <>
       <Header />
-      <main className="main" onClick={handleOutsideClick}>
+      <main className="main">
         <MetamaskNotFound isWalletInstalled={isWalletPresent} />
         {children}
         <Footer />
--- src/redux/navigation/slice.ts
+++ src/redux/navigation/slice.ts
@@ -1,22 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface OpenPopup {
-  openWallet: boolean;
   openMenu: boolean;
 }
 
 const initialState: OpenPopup = {
-  openWallet: false,
   openMenu: false,
 };
 
-export const popupOuter = createSlice({
+export const menuNavigation = createSlice({
   name: 'navigation',
   initialState,
   reducers: {
-    togglePopup: (state, action: PayloadAction<{ value: boolean }>) => {
-      state.openWallet = action.payload.value;
-    },
     toggleMenu: (state, action: PayloadAction<{ value: boolean }>) => {
       state.openMenu = action.payload.value;
     },
@@ -24,6 +19,6 @@ export const popupOuter = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { togglePopup, toggleMenu } = popupOuter.actions;
+export const { toggleMenu } = menuNavigation.actions;
 
-export default popupOuter.reducer;
+export default menuNavigation.reducer;
