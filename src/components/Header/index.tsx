a6a16c1ff3a87da65c0146936eca8daeaf75cd1d
nikmace <nikmacentosh@gmail.com>
2022-10-29 18:54:45 +0300

    Added dark mode switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ConnectKitButton } from 'connectkit';
 
-import { selectMenuOpen } from '../../redux/navigation/selectors';
 import NavLinks from './NavLinks';
+import { DarkModeSwitch } from '../Switch';
 import { toggleMenu } from '../../redux/navigation/slice';
+import { selectMenuOpen } from '../../redux/navigation/selectors';
+import { selectTheme } from '../../redux/theme/selectors';
 
 const Header = () => {
   const dispatch = useDispatch();
   const openMenu = useSelector(selectMenuOpen);
+  const theme = useSelector(selectTheme);
 
   const handleMenuToggle = () => dispatch(toggleMenu({ value: !openMenu }));
 
@@ -20,7 +23,7 @@ const Header = () => {
         <div className="header__inner">
           <Link href="/">
             <a className="header__inner-logo">
-              <img src="/images/logo.svg" alt="" />
+              <img src="/images/logo.svg" alt="Morphex Logo" />
             </a>
           </Link>
           <nav
@@ -30,6 +33,7 @@ const Header = () => {
           >
             <NavLinks />
           </nav>
+          <DarkModeSwitch />
           <div className="header__inner-user">
             <div className="header__inner-user-icon">
               <img src="/images/icons/icon-logo.svg" alt="" />
@@ -40,8 +44,19 @@ const Header = () => {
             <ConnectKitButton
               customTheme={{
                 '--ck-font-family': 'Rubik, sans-serif',
-                '--ck-connectbutton-color': '#B1B1B1',
+                '--ck-connectbutton-color':
+                  theme === 'dark' ? '#e5e7eb' : '#B1B1B1',
                 '--ck-connectbutton-font-size': '15px',
+                '--ck-connectbutton-background':
+                  theme === 'dark' ? '#334155' : '',
+                '--ck-connectbutton-hover-color':
+                  theme === 'dark' ? '#e5e7eb' : '#B1B1B1',
+                '--ck-connectbutton-hover-background':
+                  theme === 'dark' ? '#475569' : '',
+                '--ck-connectbutton-active-color':
+                  theme === 'dark' ? '#e5e7eb' : '#B1B1B1',
+                '--ck-connectbutton-active-background':
+                  theme === 'dark' ? '#475569' : '',
               }}
             />
           </div>
