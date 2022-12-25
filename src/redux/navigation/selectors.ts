1f44c823c1c7bcb90dc1885c6094b10ef37370b2
nikmace <nikmacentosh@gmail.com>
2022-12-25 22:22:24 +0200

    Updated redux logic for pages

--- src/redux/navigation/selectors.ts
+++ src/redux/navigation/selectors.ts
@@ -2,3 +2,9 @@ import { RootState } from '../store';
 
 export const selectMenuOpen = (state: RootState) =>
   state.menuNavigation.openMenu;
+
+export const selectSettings = (state: RootState) =>
+  state.menuNavigation.openSettings;
+
+export const selectMoreTab = (state: RootState) =>
+  state.menuNavigation.openMoreTab;
--- src/redux/navigation/slice.ts
+++ src/redux/navigation/slice.ts
@@ -2,10 +2,14 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface OpenPopup {
   openMenu: boolean;
+  openSettings: boolean;
+  openMoreTab: boolean;
 }
 
 const initialState: OpenPopup = {
   openMenu: false,
+  openSettings: false,
+  openMoreTab: false,
 };
 
 export const menuNavigation = createSlice({
@@ -15,10 +19,17 @@ export const menuNavigation = createSlice({
     toggleMenu: (state, action: PayloadAction<{ value: boolean }>) => {
       state.openMenu = action.payload.value;
     },
+    toggleSettings: (state, action: PayloadAction<{ value: boolean }>) => {
+      state.openSettings = action.payload.value;
+    },
+    toggleMoreTab: (state, action: PayloadAction<{ value: boolean }>) => {
+      state.openMoreTab = action.payload.value;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { toggleMenu } = menuNavigation.actions;
+export const { toggleMenu, toggleSettings, toggleMoreTab } =
+  menuNavigation.actions;
 
 export default menuNavigation.reducer;
