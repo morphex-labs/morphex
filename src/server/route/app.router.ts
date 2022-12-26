43300c8b14b42458999015da39389ad7251fd777
nikmace <nikmacentosh@gmail.com>
2022-12-26 16:18:27 +0200

    removed files

--- src/server/route/app.router.ts
+++
@@ -1,6 +0,0 @@
-import { createRouter } from '../createRouter';
-import { someRouter } from './some.router';
-
-export const appRouter = createRouter().merge('some.', someRouter);
-
-export type AppRouter = typeof appRouter;
