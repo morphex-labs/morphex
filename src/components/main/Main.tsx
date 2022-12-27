41948c0ce2210ace1b4f0ebac85999f041f0b683
nikmace <nikmacentosh@gmail.com>
2022-12-27 10:11:19 +0200

    Added new ticker widget and Analytics chart

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,17 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+import {
+  AdvancedRealTimeChart,
+  CopyrightStyles,
+  Ticker,
+} from 'react-ts-tradingview-widgets';
 
 import { selectTheme } from '../../redux/theme/selectors';
 
 export default function Main() {
   const theme = useSelector(selectTheme);
 
+  const styles: CopyrightStyles = {
+    parent: {
+      display: 'none',
+    },
+  };
+
   return (
     <div className="tradeMain">
       <div className="tradeMain__header">
-        <div className="button primary">ETH / USD</div>
+        {/* <div className="button primary">ETH / USD</div>
         <div className="tradeMain__header-info">
           <h6 className="xsm">Price</h6>
           <p className="xsm">$1,630.70</p>
@@ -27,15 +37,36 @@ export default function Main() {
         <div className="tradeMain__header-info">
           <h6 className="xsm">24H Low</h6>
           <p className="xsm">1,623.70</p>
-        </div>
+        </div> */}
+        <Ticker
+          colorTheme={theme === 'dark' ? 'dark' : 'light'}
+          isTransparent
+          showSymbolLogo
+          locale="en"
+          symbols={[
+            {
+              proName: 'BINANCE:FTMUSD',
+              title: 'Fantom',
+            },
+            {
+              proName: 'BITSTAMP:BTCUSD',
+              title: 'Bitcoin',
+            },
+            {
+              proName: 'BITSTAMP:ETHUSD',
+              title: 'Ethereum',
+            },
+          ]}
+          copyrightStyles={styles}
+        />
       </div>
       {/* <ChartComponent /> */}
       <div id="tradingview-graph1" className="tradeMain__graph">
-        <TradingViewWidget
-          symbol="BINANCE:BTCUSDT"
-          theme={theme === 'dark' ? Themes.DARK : Themes.LIGHT}
+        <AdvancedRealTimeChart
+          theme={theme === 'dark' ? 'dark' : 'light'}
           locale="en"
           autosize
+          symbol="BINANCE:FTMUSD"
           interval="240"
           toolbar_bg="#f1f3f6"
           withdateranges
@@ -44,6 +75,7 @@ export default function Main() {
           show_popup_button
           popup_width="1000"
           popup_height="650"
+          copyrightStyles={styles}
         />
       </div>
     </div>
