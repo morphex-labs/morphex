8c38e7ca77e5075dfc84649972610e54b0245c9b
nikmace <nikmacentosh@gmail.com>
2022-12-27 15:57:25 +0200

    Generic button for checking if wallet is connected

---
+++ src/components/buttons/GenericBtn.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React from 'react';
+import { useAccount } from 'wagmi';
+
+import ConnectWalletBtn from './ConnectWalletBtn';
+
+export interface GenericBtnProps {
+  btnTextMain: string;
+  classNamesConnect: string;
+  classNamesMain: string;
+  onClickFunc?: () => void;
+}
+
+const GenericBtn: React.FC<GenericBtnProps> = ({
+  btnTextMain,
+  classNamesConnect,
+  classNamesMain,
+  onClickFunc,
+}) => {
+  const { isConnected } = useAccount();
+
+  if (!isConnected) return <ConnectWalletBtn classNames={classNamesConnect} />;
+
+  return (
+    <button type="button" className={classNamesMain} onClick={onClickFunc}>
+      {btnTextMain}
+    </button>
+  );
+};
+
+export default GenericBtn;
