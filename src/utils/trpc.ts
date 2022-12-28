6c6372f8ac07163b268fab5905a34816b7822d63
nikmace <nikmacentosh@gmail.com>
2022-12-28 12:45:18 +0200

    Removed rules

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import superjson from 'superjson';
 import { httpBatchLink, loggerLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
