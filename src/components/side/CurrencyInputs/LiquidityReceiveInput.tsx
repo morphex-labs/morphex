e286e4a10301b0e36bb38ff0c33a4d2600bbef10
nikmace <nikmacentosh@gmail.com>
2022-12-29 12:02:36 +0200

    Liquidity pay and receive inputs

---
+++ src/components/side/CurrencyInputs/LiquidityReceiveInput.tsx
@@ -0,0 +1,51 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+import { InputProps } from './Input';
+
+const LiquidityReceiveInput: React.FC<InputProps> = ({
+  label,
+  setValue,
+  value,
+}) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+      exit={{ opacity: 0, y: 10 }}
+      className="liquidityTab__body"
+    >
+      <div className="input__outer">
+        <label htmlFor="pay-id">
+          <span id="pay-id">Pay: $0.00</span> <span>Balance: 0.00</span>
+        </label>
+        <div className="input">
+          <input type="number" placeholder="0.0" />
+          <div className="input__btns">
+            <button type="button" className="input__max">
+              MAX
+            </button>
+            <div className="input__coin">ETH</div>
+          </div>
+        </div>
+      </div>
+      <div className="input__outer">
+        <label htmlFor="receive-id">
+          <span id="receive-id">Receive: $0.00</span> <span>Balance: 0.00</span>
+        </label>
+        <div className="input">
+          <input type="number" placeholder="0.0" />
+          <div className="input__btns">
+            <button type="button" className="input__max">
+              MAX
+            </button>
+            <div className="input__coin">MLP</div>
+          </div>
+        </div>
+      </div>
+      <p className="xsm">Fees: 0.00</p>
+    </motion.div>
+  );
+};
+
+export default LiquidityReceiveInput;
