f8d2492cf063887d4346ef63e08b04dcee40abf2
nikmace <nikmacentosh@gmail.com>
2022-12-29 13:50:07 +0200

    Added liquidity section functionality

--- src/components/liquidity/LiquidityTab.tsx
+++ src/components/liquidity/LiquidityTab.tsx
@@ -1,13 +1,52 @@
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { selectAllCurrencies } from '../../redux/currency-selector/selectors';
 
+import { mock } from '../../utils/mockRequest';
 import GenericBtn from '../buttons/GenericBtn';
-import LiquidityPayInput from '../side/CurrencyInputs/LiquidityPayInput';
-import LiquidityReceiveInput from '../side/CurrencyInputs/LiquidityReceiveInput';
+import LiquidityMintInput from '../side/CurrencyInputs/LiquidityMintInput';
+import LiquidityRedeemInput from '../side/CurrencyInputs/LiquidityRedeemInput';
 
 export default function LiquidityTab() {
-  const [state, setState] = useState('mint');
-  const [coin1, setCoin1] = useState('');
-  const [coin2, setCoin2] = useState('');
+  const [state, setState] = useState<string>('mint');
+  const [mintCoin1, setMintCoin1] = useState<string>('');
+  const [mintCoin2, setMintCoin2] = useState<string>('');
+  const [redeemCoin1, setRedeemCoin1] = useState<string>('');
+  const [redeemCoin2, setRedeemCoin2] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    payCurrency: { symbol: paySymbol },
+  } = useSelector(selectAllCurrencies);
+
+  const resetFields = () => {
+    setMintCoin1('');
+    setMintCoin2('');
+    setRedeemCoin1('');
+    setRedeemCoin2('');
+  };
+
+  const handleConfirm = () => {
+    // Test function to show all values
+    resetFields();
+    setLoading(true);
+    toast.promise(mock(true, 2000), {
+      success: () => {
+        setLoading(false);
+        return `Transactions confirmed with values:
+        Type: ${state}
+        Pay: ${paySymbol} $${state === 'mint' ? mintCoin1 : redeemCoin1}
+        Receive: $${state === 'mint' ? mintCoin2 : redeemCoin2}
+        `;
+      },
+      error: () => {
+        setLoading(false);
+        return 'Transaction failed!';
+      },
+      loading: 'Confirming your transaction... please wait.',
+    });
+  };
 
   return (
     <div className="liquidityTab">
@@ -28,20 +67,30 @@ export default function LiquidityTab() {
         </button>
       </div>
       {state === 'mint' && (
-        <LiquidityPayInput label="Pay" setValue={setCoin1} value={coin1} />
+        <LiquidityMintInput
+          label="Pay"
+          setValue={setMintCoin1}
+          value={mintCoin1}
+          setValue2={setMintCoin2}
+          value2={mintCoin2}
+        />
       )}
       {state === 'redeem' && (
-        <LiquidityReceiveInput
-          label="Receive"
-          setValue={setCoin2}
-          value={coin2}
+        <LiquidityRedeemInput
+          label="Pay"
+          setValue={setRedeemCoin1}
+          setValue2={setRedeemCoin2}
+          value={redeemCoin1}
+          value2={redeemCoin2}
         />
       )}
       <div className="liquidityTab__footer">
         <GenericBtn
           btnTextMain="Confirm"
-          classNamesMain="button primary sm"
+          classNamesMain={`button primary sm ${loading ? 'disabledBtn' : ''}`}
           classNamesConnect="button primary sm"
+          onClickFunc={handleConfirm}
+          disabled={loading}
         />
       </div>
     </div>
