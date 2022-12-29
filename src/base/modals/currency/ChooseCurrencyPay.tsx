b77daba66513f66c065c769952f92bc80665d451
nikmace <nikmacentosh@gmail.com>
2022-12-29 10:52:21 +0200

    Pay modal

---
+++ src/base/modals/currency/ChooseCurrencyPay.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { payCurrencies } from '../../../constants';
+import { setPayCurrency } from '../../../redux/currency-selector/slice';
+import { RootState } from '../../../redux/store';
+import Modal from '../Modal';
+
+export default function ChooseCurrencyPay({
+  closeFunc,
+}: {
+  closeFunc: () => void;
+}) {
+  const dispatch = useDispatch();
+  const selectedCurrency = useSelector(
+    (state: RootState) => state.selectCurrency.payCurrency
+  );
+
+  const handleChangeCurrency = (symbol: string, name: string) => {
+    closeFunc();
+    setTimeout(() => {
+      dispatch(setPayCurrency({ name, symbol }));
+    }, 200);
+  };
+
+  return (
+    <Modal title="Choose Currency - Pay" closeFunc={closeFunc}>
+      <div className="currencies">
+        {payCurrencies
+          .filter((v) => v.symbol !== selectedCurrency.symbol)
+          .map((currency) => {
+            return (
+              <button
+                type="button"
+                className="button secondary currencies-item"
+                onClick={() =>
+                  handleChangeCurrency(currency.symbol, currency.name)
+                }
+              >
+                {currency.symbol} - {currency.name}
+              </button>
+            );
+          })}
+      </div>
+    </Modal>
+  );
+}
