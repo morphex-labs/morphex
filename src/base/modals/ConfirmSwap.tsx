b1a8948726eecc00b06b6d5ae5b512c12a61d4a2
nikmace <nikmacentosh@gmail.com>
2022-12-29 15:33:48 +0200

    Confirm swap market/limit modal

--- src/base/modals/ConfirmSwap.tsx
+++ src/base/modals/ConfirmSwap.tsx
@@ -1,9 +1,58 @@
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+
 import Modal from './Modal';
 import { arrowBottom } from '../SVG';
+import { selectAllCurrencies } from '../../redux/currency-selector/selectors';
+import { mock } from '../../utils/mockRequest';
+import GenericBtn from '../../components/buttons/GenericBtn';
 
-export default function ConfirmSwap({ closeFunc }: { closeFunc: () => void }) {
+interface ConfirmProps {
+  closeFunc: () => void;
+  pay: string;
+  short: string;
+  type: string;
+}
+
+export default function ConfirmSwap({
+  closeFunc,
+  short,
+  pay,
+  type,
+}: ConfirmProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    shortCurrency: { symbol: shortSymbol, name: shortName },
+    payCurrency: { symbol: paySymbol, name: payName },
+  } = useSelector(selectAllCurrencies);
+
+  const handleConfirmOpen = () => {
+    // Test function to show all values
+    closeFunc();
+    setLoading(true);
+    toast.promise(mock(true, 2000), {
+      success: () => {
+        setLoading(false);
+        return `Transactions confirmed with values:
+        Type: ${type}
+        Pay: ${payName} - ${paySymbol} amount $${pay}
+        Long: ${shortName} - ${shortSymbol} amount $${short}
+      `;
+      },
+      error: () => {
+        setLoading(false);
+        return 'Transaction failed!';
+      },
+      loading: 'Confirming your transaction... please wait.',
+    });
+  };
   return (
-    <Modal title="Confirm Swap" closeFunc={closeFunc}>
+    <Modal
+      title={type === 'market' ? 'Confirm Swap' : 'Confirm Limit Order - Swap'}
+      closeFunc={closeFunc}
+    >
       <div className="modalTable">
         <div className="modalTable__row">
           <h6 className="xsm">Pay</h6>
@@ -42,9 +91,13 @@ export default function ConfirmSwap({ closeFunc }: { closeFunc: () => void }) {
         </div>
       </div>
       <div className="modal__btn">
-        <button type="button" className="button primary sm">
-          Confirm Swap
-        </button>
+        <GenericBtn
+          btnTextMain="Confirm Swap"
+          classNamesMain={`button primary sm ${loading ? 'disabledBtn' : ''}`}
+          classNamesConnect="button primary sm"
+          onClickFunc={handleConfirmOpen}
+          disabled={loading}
+        />
       </div>
     </Modal>
   );
