dcc9fa3c772342223608652839fbe80014908f0b
nikmace <nikmacentosh@gmail.com>
2022-12-29 12:01:28 +0200

    Added new price input component

---
+++ src/components/side/CurrencyInputs/PriceInputs.tsx
@@ -0,0 +1,24 @@
+import { InputProps } from './Input';
+
+const PriceInput = ({ label, value, setValue }: InputProps) => {
+  const markPrice = 100;
+  return (
+    <div className="input__outer">
+      <p className="xsm" />
+      <label htmlFor="none">
+        {label}
+        <span>Mark: ${markPrice}</span>
+      </label>
+      <div className="input ">
+        <input
+          type="number"
+          placeholder="0.0"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PriceInput;
