4c8d276560f56f5c184aa51f7ac841f81e9d3dde
nikmace <nikmacentosh@gmail.com>
2022-12-30 20:28:10 +0200

    Refactored code, added apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,58 +1,54 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import type { AppProps } from 'next/app';
+
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import { WagmiConfig, createClient } from 'wagmi';
-import { fantom, fantomTestnet } from 'wagmi/chains';
-import { getDefaultClient } from 'connectkit';
+import { WagmiConfig } from 'wagmi';
+import { ApolloProvider } from '@apollo/client';
 
 import '../scss/main.scss';
-import { store } from '../redux/store';
-import Layout from '../components/Layout';
 import { trpc } from '../utils/trpc';
-
-// Creating the web3 client in wagmi, using "getDefaultClient" from ConnectKit which simplifies for us.
-const client = createClient(
-  getDefaultClient({
-    appName: 'Morphex',
-    chains: [fantom, fantomTestnet],
-  })
-);
+import { store } from '../redux/store';
+import web3Client from '../wagmi/client';
+import Layout from '../components/Layout';
+import { avalancheGraphClient, chainlinkClient } from '../subgraph/clients';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <WagmiConfig client={client}>
-        <Head>
-          <title>Morphex</title>
-          <meta name="description" content="Morphex Swap - Lend & Borrow" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link rel="icon" href="/favicon.svg" />
-        </Head>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            className: '',
-            duration: 4000,
-            success: {
-              style: {
-                padding: '8px',
-                fontSize: '12px',
+    <ApolloProvider client={chainlinkClient}>
+      <Provider store={store}>
+        <WagmiConfig client={web3Client}>
+          <Head>
+            <title>Morphex</title>
+            <meta name="description" content="Morphex Swap - Lend & Borrow" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <link rel="icon" href="/favicon.svg" />
+          </Head>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              className: '',
+              duration: 4000,
+              success: {
+                style: {
+                  padding: '8px',
+                  fontSize: '12px',
+                },
               },
-            },
-          }}
-        />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </WagmiConfig>
-    </Provider>
+            }}
+          />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </WagmiConfig>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
